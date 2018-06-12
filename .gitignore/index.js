999c2bb581863bbf03967fe728e33c1ebddd7411
MehmetSalihK <37173211+MehmetSalihK@users.noreply.github.com>
2018-06-13 00:22:42 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const TOKEN = "NDUzNjIxNDUwNzc1OTg2MTc2.DfhjeQ.T_F3AELdZiLkaCNsiB67rA6QuiI";
+const TOKEN = "NDM4MDg1MzYxOTczODU0MjEw.DgHWKA.utyndAxtfcqMTRbVLX2WIqKcK4A";
 const ownerID = "174565176492687361"
 const PREFIX = "-";
 
@@ -9,13 +9,52 @@ var bot = new Discord.Client();
 
 var servers = {};
 
-bot.on("guildMemberAdd", member => {
-    member.guild.channels.find("name", "📈à-rejoin📈").send(`**Bienvenue à** __**${member.guild.name}**__ **maintenant** __**${member.user.username}**__ **est avec nous!**`)
-})
+bot.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', '📈à-rejoin📈');
+    let memberavatar = member.user.avatarURL
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':bust_in_silhouette: | Participant dans notre serveur : ', `${member}`)
+        .addField(':microphone2: | Bienvenue!', `Bienvenue sur notre serveur, ${member}`)
+        .addField(':id: | Utilisateur :', "**[" + `${member.id}` + "]**")
+        .addField(':family_mwgb: | Vous êtes ', `${member.guild.memberCount}` + "ième personnes")
+        .addField("Nom", `<@` + `${member.id}` + `>`, true)
+        .addField('Serveur', `${member.guild.name}`, true )
+        .setFooter(` Pour la commande d'aide [-aide]`, "https://imgur.com/rAgsGxu.png")
+        .setTimestamp()
 
-bot.on("guildMemberRemove", member => {
-    member.guild.channels.find("name", "📉à-quitter📉").send(`__**${member.user.username}**__ **n'est plus sur notre serveur** __**${member.guild.name}**__`)
-})
+        channel.sendEmbed(embed);
+});
+
+bot.on('guildMemberAdd', member => {
+
+    console.log(`${member}`, "relié" + `${member.guild.name}`)
+
+});
+
+bot.on('guildMemberRemove', member => {
+    let channel = member.guild.channels.find('name', '📉à-quitter📉');
+    let memberavatar = member.user.avatarURL
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField('Nom:', `${member}`)
+        .addField('Pourquoi ?', "qui t'a soutenu ?")
+        .addField('Bye Bye :(', 'Ouvre ton chemin, mon frère/ma soeur!')
+        .addField('Maintenant sur notre serveur ils nous reste', `${member.guild.memberCount}` + " personnes")
+        .setFooter(` Pour la commande d'aide [-aide]`, "https://imgur.com/rAgsGxu.png")
+        .setTimestamp()
+
+        channel.sendEmbed(embed);
+});
+
+bot.on('guildMemberRemove', member => {
+    console.log(`${member}` + "ont été divisés" + `${member.guild.name}` + "Laissez un message maintenant")
+    console.log("Message envoyé")
+});
 
 bot.on('guildMemberAdd', member => {
     var role = member.guild.roles.find("name", "✓NDNG✓");
